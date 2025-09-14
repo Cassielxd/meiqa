fd8d5ea4a2df07f33d3fde3c23c3df2e47e421b9
Cassie <348040933@qq.com>
2025-09-14 18:19:14 +0800

    init

---
+++ template/tenant/src/api/account.js
@@ -0,0 +1,80 @@
+
+
+import request from '@/libs/request'
+
+/*
+ * 登录
+ * */
+export function AccountLogin(data) {
+    return request({
+        url: '/login',
+        method: 'post',
+        data
+    })
+}
+
+
+
+
+/**
+ * 退出登陆
+ * @constructor
+ */
+export function AccountLogout() {
+    return request({
+        url: '/setting/admin/logout',
+        method: 'get'
+    })
+}
+
+/*
+* 获取验证码图片
+*/
+
+export function captcha_pro() {
+    return request({
+        url: '/captcha_pro',
+        method: 'get'
+    })
+}
+
+/**
+ * 获取轮播图和logo
+ */
+export function loginInfoApi() {
+    return request({
+        url: '/login/info',
+        method: 'get'
+    })
+}
+
+/**
+ * 获取菜单数据
+ */
+export function menusApi() {
+    return request({
+        url: '/menus',
+        method: 'get'
+    })
+}
+
+/**
+ * 搜索菜单数据
+ */
+export function menusListApi() {
+    return request({
+        url: '/menusList',
+        method: 'get'
+    })
+}
+
+/**
+ * 租户注册
+ */
+export function AccountRegister(data) {
+    return request({
+        url: '/tenant/register',
+        method: 'post',
+        data
+    })
+}
---
+++ template/tenant/src/api/index.js
@@ -0,0 +1,62 @@
+
+
+import request from '@/libs/request'
+
+/**
+ * @description 首页订单图表
+ */
+export function orderApi (params) {
+    return request({
+        url: 'home/order',
+        method: 'get',
+        params
+    })
+}
+
+/**
+ * @description 首页订单图表
+ */
+export function userApi () {
+    return request({
+        url: 'home/user',
+        method: 'get'
+    })
+}
+
+/**
+ * @description 首页统计
+ */
+export function sumApi () {
+    return request({
+        url: 'chart/sum',
+        method: 'get'
+    })
+}
+
+/**
+ * @description 首页统计图表
+ */
+export function charApi (params) {
+    return request({
+        url: 'chart',
+        method: 'get',
+        params
+    })
+}
+
+/**
+ * @description 首页商品交易额排行
+ */
+export function rankApi () {
+    return request({
+        url: 'home/rank',
+        method: 'get'
+    })
+}
+
+export function checkAuth () {
+    return request({
+        url: 'check_auth',
+        method: 'get'
+    })
+}
