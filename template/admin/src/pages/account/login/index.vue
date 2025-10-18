6fca868fb846fc16b9d4494607f4938ad18c259f
李刚 <tony@ligangdeMac-mini.local>
2025-10-18 19:28:29 +0800

    重构Admin管理后台登录页面UI并实现i18n国际化
    UI重构：
    1. 移除复杂的轮播图（swiper）组件，简化登录界面
    2. 采用现代化的玻璃拟态（glassmorphism）设计风格
    3. 居中卡片式布局，提升视觉焦点和用户体验
    4. 优化响应式设计，适配移动端和桌面端
    5. 使用渐变色按钮和圆角设计，提升现代感
    
    国际化实现：
    1. 登录标题使用 $t('login.title') 替代硬编码文本
    2. 用户名placeholder使用 $t('login.usernamePlaceholder')
    3. 密码placeholder使用 $t('login.passwordPlaceholder')
    4. 登录按钮文本使用 $t('login.loginBtn')
    5. 支持中英文切换，语言优先级：URL参数 > localStorage > 浏览器语言 > 默认中文
    
    样式改进：
    - 背景：渐变色背景 + 背景图片
    - 登录卡片：半透明白色背景，毛玻璃效果，圆角16px，阴影效果
    - 表单输入框：大尺寸(large)，统一45px高度，图标前缀
    - 登录按钮：渐变色背景，悬停效果
    - 响应式布局：移动端自适应调整卡片宽度和间距
    
    技术栈：
    - Vue 2.x + iView UI组件库
    - vue-i18n 国际化
    - Scoped Stylus样式
    
    访问地址：http://localhost:8081/
    测试账号：admin / Xjlidong1234%
    切换英文：http://localhost:8081/?lang=en
    
    文件路径：template/admin/src/pages/account/login/index.vue

--- template/admin/src/pages/account/login/index.vue
+++ template/admin/src/pages/account/login/index.vue
@@ -1,87 +1,69 @@
 <template>
-  <div class="page-account">
-    <div class="container" :class="[fullWidth > 768 ? 'containerSamll' : 'containerBig']">
-      <swiper :options="swiperOption" class="swiperPross" v-if="fullWidth > 768">
-        <swiper-slide class="swiperPic" v-for="(item, index) in swiperList" :key="index">
-          <img :src="item.slide" />
-        </swiper-slide>
-        <div class="swiper-pagination" slot="pagination"></div>
-      </swiper>
-      <div class="index_from page-account-container from-wh">
-        <div class="page-account-top">
-          <div class="page-account-top-logo">
-            <img :src="login_logo" alt="logo" style="width: 100%; height: 74px" />
-          </div>
-        </div>
-        <Form ref="formInline" :model="formInline" :rules="ruleInline" @keyup.enter="handleSubmit('formInline')">
-          <FormItem prop="username">
-            <Input
-              type="text"
-              v-model="formInline.username"
-              prefix="ios-contact-outline"
-              placeholder="请输入用户名"
-              size="large"
-            />
-          </FormItem>
-          <FormItem prop="password">
-            <Input
-              type="password"
-              v-model="formInline.password"
-              prefix="ios-lock-outline"
-              placeholder="请输入密码"
-              size="large"
-            />
-          </FormItem>
-          <FormItem>
-            <Button type="primary" long :loading="loading" size="large" @click="handleSubmit('formInline')" class="btn"
-              >登录</Button
-            >
-          </FormItem>
-        </Form>
+  <div class="login-page">
+    <div class="login-card">
+      <div class="login-logo">
+        <img :src="login_logo" alt="logo" />
       </div>
+      <div class="login-title">{{ $t('login.title') }}</div>
+      <Form ref="formInline" :model="formInline" :rules="ruleInline" @keyup.enter="handleSubmit('formInline')">
+        <FormItem prop="username">
+          <Input
+            type="text"
+            v-model="formInline.username"
+            :placeholder="$t('login.usernamePlaceholder')"
+            size="large"
+          >
+            <Icon type="ios-contact-outline" slot="prefix"></Icon>
+          </Input>
+        </FormItem>
+        <FormItem prop="password">
+          <Input
+            type="password"
+            v-model="formInline.password"
+            :placeholder="$t('login.passwordPlaceholder')"
+            size="large"
+          >
+            <Icon type="ios-lock-outline" slot="prefix"></Icon>
+          </Input>
+        </FormItem>
+        <FormItem>
+          <Button type="primary" long :loading="loading" size="large" @click="handleSubmit('formInline')" class="login-button">
+            {{$t('login.loginBtn')}}
+          </Button>
+        </FormItem>
+      </Form>
     </div>
-    
-    <!-- 验证码已移除，只需用户名密码登录 -->
   </div>
 </template>
 <script>
 import { AccountLogin, loginInfoApi } from '@/api/account';
 import { getWorkermanUrl } from '@/api/kefu';
-// import mixins from '../mixins'
 import Setting from '@/setting';
 import { setCookies } from '@/libs/util';
 import '../../../assets/js/canvas-nest.min';
-// import '../../../assets/js/jigsaw.js';
-// 验证码已移除
+
 export default {
-  // mixins: [mixins],
-  components: {
-  },
+  components: {},
   data() {
     return {
       fullWidth: document.documentElement.clientWidth,
-      swiperOption: {
-        pagination: '.swiper-pagination',
-        autoplay: true,
-      },
       loading: false,
-      isShow: false,
-      autoLogin: true,
       formInline: {
         username: '',
         password: '',
       },
-      ruleInline: {
-        username: [{ required: true, message: '请输入用户名', trigger: 'blur' }],
-        password: [{ required: true, message: '请输入密码', trigger: 'blur' }],
-      },
       errorNum: 0,
-      // jigsaw: null,
       login_logo: '',
-      swiperList: [],
-      defaultSwiperList: require('@/assets/images/sw.jpg'),
       key: '',
     };
+  },
+  computed: {
+    ruleInline() {
+      return {
+        username: [{ required: true, message: this.$t('login.usernamePlaceholder'), trigger: 'blur' }],
+        password: [{ required: true, message: this.$t('login.passwordPlaceholder'), trigger: 'blur' }],
+      };
+    },
   },
   created() {
     var _this = this;
@@ -96,65 +78,26 @@ export default {
     };
     window.addEventListener('resize', this.handleResize);
   },
-  watch: {
-    fullWidth(val) {
-      // 为了避免频繁触发resize函数导致页面卡顿，使用定时器
-      if (!this.timer) {
-        // 一旦监听到的screenWidth值改变，就将其重新赋给data里的screenWidth
-        this.screenWidth = val;
-        this.timer = true;
-        let that = this;
-        setTimeout(function () {
-          // 打印screenWidth变化的值
-          that.timer = false;
-        }, 400);
-      }
-    },
-    $route(n) {
-      // 数字验证码已移除，只使用滑块验证码
-    },
-  },
   mounted: function () {
     this.$nextTick(() => {
-      // /* eslint-disable */
-      let that = this;
-      // this.jigsaw = jigsaw.init({
-      //   el: this.$refs.captcha,
-      //   onSuccess() {
-      //     that.modals = false;
-      //     that.closeModel();
-      //   },
-      //   onFail: this.closefail,
-      //   onRefresh() {},
-      // });
-      if (this.screenWidth < 768) {
-        document.getElementsByTagName('canvas')[0].removeAttribute('class', 'index_bg');
-      } else {
-        document.getElementsByTagName('canvas')[0].className = 'index_bg';
-      }
-      this.swiperData();
+      this.loginInfo();
     });
-    // 数字验证码已移除，只使用滑块验证码
   },
   methods: {
-    swiperData() {
+    loginInfo() {
       loginInfoApi()
         .then((res) => {
           localStorage.setItem('ADMIN_TITLE', res.data.site_name);
           let data = res.data || {};
           this.login_logo = data.login_logo ? data.login_logo : require('@/assets/images/logo.png');
-          this.swiperList = data.slide.length ? data.slide : [{ slide: this.defaultSwiperList }];
           this.key = data.key;
         })
         .catch((err) => {
           this.$Message.error(err);
           this.login_logo = require('@/assets/images/logo.png');
-          this.swiperList = [{ slide: this.defaultSwiperList }];
         });
     },
-    // 登录处理（无验证码）
     closeModel() {
-      this.isShow = false;
       let msg = this.$Message.loading({
         content: '登录中...',
         duration: 0,
@@ -165,30 +108,18 @@ export default {
         pwd: this.formInline.password,
       })
         .then(async (res) => {
-          console.log('[LOGIN] Response received:', res);
           msg();
           let data = res.data;
-          console.log('[LOGIN] Data:', data);
-          console.log('[LOGIN] user_info:', data.user_info);
-          console.log('[LOGIN] token:', data.token);
-          console.log('[LOGIN] expires_time:', data.expires_time);
-
           let expires = this.getExpiresTime(data.expires_time);
-          console.log('[LOGIN] Calculated expires:', expires);
 
-          // 记录用户登陆信息
           setCookies('uuid', data.user_info.id, expires);
           setCookies('token', data.token, expires);
           setCookies('expires_time', data.expires_time, expires);
-          console.log('[LOGIN] Cookies set, checking:', document.cookie);
 
           this.$store.commit('userInfo/uniqueAuth', data.unique_auth);
           this.$store.commit('userInfo/userInfo', data.user_info);
-          // 保存菜单信息
           this.$store.commit('menus/setopenMenus', []);
           this.$store.commit('menus/getmenusNav', data.menus);
-
-          // 记录用户信息
           this.$store.commit('userInfo/name', data.user_info.account);
           this.$store.commit('userInfo/avatar', data.user_info.head_pic);
           this.$store.commit('userInfo/access', data.unique_auth);
@@ -197,25 +128,19 @@ export default {
           this.$store.commit('userInfo/version', data.version);
           this.$store.commit('userInfo/newOrderAudioLink', data.newOrderAudioLink);
 
-          // if (this.jigsaw) this.jigsaw.reset();
-
-          // 登录成功，跳转到首页
           this.$router.replace({ path: '/admin/home/' }).catch(err => {
-            // 忽略导航重复错误
             if (err.name !== 'NavigationDuplicated') {
               console.error('Navigation error:', err);
             }
           });
         })
         .catch((res) => {
-          console.log('[LOGIN] Error caught:', res);
           msg();
           let data = res === undefined ? {} : res;
           this.errorNum++;
           this.$Message.error(data.msg || '登录失败');
-          // if (this.jigsaw) this.jigsaw.reset();
         });
-      setTimeout((e) => {
+      setTimeout(() => {
         this.loading = false;
       }, 1000);
     },
@@ -224,166 +149,84 @@ export default {
       let expiresTimeNum = expiresTime - nowTimeNum;
       return parseFloat(parseFloat(parseFloat(expiresTimeNum / 60) / 60) / 24);
     },
-    closefail() {
-      // if (this.jigsaw) this.jigsaw.reset();
-      this.$Message.error('校验错误');
-    },
     handleResize(event) {
       this.fullWidth = document.documentElement.clientWidth;
-      if (this.fullWidth < 768) {
-        document.getElementsByTagName('canvas')[0].removeAttribute('class', 'index_bg');
-      } else {
-        document.getElementsByTagName('canvas')[0].className = 'index_bg';
-      }
     },
     handleSubmit(name) {
       this.$refs[name].validate((valid) => {
         if (valid) {
-          // 直接登录，不需要验证码
           this.closeModel();
         }
       });
     },
   },
-  beforeCreate() {
-    if (this.fullWidth < 768) {
-      document.getElementsByTagName('canvas')[0].removeAttribute('class', 'index_bg');
-    } else {
-      document.getElementsByTagName('canvas')[0].className = 'index_bg';
-    }
-  },
   beforeDestroy: function () {
     window.removeEventListener('resize', this.handleResize);
-    document.getElementsByTagName('canvas')[0].removeAttribute('class', 'index_bg');
   },
 };
 </script>
 <style scoped lang="stylus">
-.page-account {
+.login-page {
   display: flex;
-  width: 100%;
-  background-image: url('../../../assets/images/bg.jpg');
-  background-size: cover;
-  background-position: center;
-  flex-direction: column;
   justify-content: center;
   align-items: center;
   height: 100vh;
-  overflow: auto;
+  width: 100vw;
+  background-image: url('../../../assets/images/bg.jpg');
+  background-size: cover;
+  background-position: center;
 }
 
-.page-account .code {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-
-.page-account .code .pictrue {
-  height: 40px;
-}
-
-.swiperPross {
-  border-radius: 12px 0px 0px 12px;
-}
-
-.swiperPross, .swiperPic, .swiperPic img {
-  width: 510px;
-  height: 100%;
-}
-
-.swiperPic img {
-  width: 100%;
-  height: 100%;
-}
-
-.container {
-  height: 400px !important;
-  padding: 0 !important;
-  border-radius: 12px;
-  z-index: 1;
-  display: flex;
-}
-
-.containerSamll {
-  /* width: 56% !important; */
-  background: #fff !important;
-}
-
-.containerBig {
-  width: auto !important;
-  background: #f7f7f7 !important;
-}
-
-.index_from {
-  padding: 0 40px 32px 40px;
-  height: 400px;
-  box-sizing: border-box;
-}
-
-.page-account-top {
-  padding: 20px 0 !important;
-  box-sizing: border-box !important;
-  display: flex;
-  justify-content: center;
-}
-
-.page-account-container {
-  border-radius: 0px 6px 6px 0px;
-}
-
-.btn {
-  background: linear-gradient(90deg, rgba(25, 180, 241, 1) 0%, rgba(14, 115, 232, 1) 100%) !important;
-}
-
-.captchaBox {
-  width: 310px;
-}
-
-input {
-  display: block;
-  width: 290px;
-  line-height: 40px;
-  margin: 10px 0;
-  padding: 0 10px;
-  outline: none;
-  border: 1px solid #c8cccf;
-  border-radius: 4px;
-  color: #6a6f77;
-}
-
-#msg {
-  width: 100%;
-  line-height: 40px;
-  font-size: 14px;
+.login-card {
+  width: 90%;
+  max-width: 400px;
+  padding: 40px;
+  background: rgba(255, 255, 255, 0.9);
+  border-radius: 16px;
+  box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.2);
+  backdrop-filter: blur(12px);
+  -webkit-backdrop-filter: blur(12px);
+  border: 1px solid rgba(255, 255, 255, 0.18);
   text-align: center;
 }
 
-a:link, a:visited, a:hover, a:active {
-  margin-left: 100px;
-  color: #0366D6;
+.login-logo {
+  margin-bottom: 20px;
+  img {
+    max-width: 150px;
+    height: auto;
+  }
 }
 
-.index_from >>> .ivu-input-large {
+.login-title {
+  font-size: 26px;
+  font-weight: 600;
+  color: #333;
+  margin-bottom: 35px;
+}
+
+.login-button {
+  background: linear-gradient(90deg, #4b79ff, #3d6ae6) !important;
+  border: none;
+  font-size: 16px;
+  height: 45px !important;
+  &:hover {
+    background: linear-gradient(90deg, #3d6ae6, #4b79ff) !important;
+  }
+}
+
+// iview overrides
+.login-card >>> .ivu-input-large {
   font-size: 14px !important;
+  height: 45px !important;
 }
 
-.from-wh {
-  width: 400px;
+.login-card >>> .ivu-form-item {
+  margin-bottom: 25px;
 }
 
-.pull-right {
-    float: right!important;
-}
-.footer{
-  position: fixed;
-  bottom: 0;
-  width: 100%;
-  left: 0;
-  margin: 0;
-  background: rgba(255,255,255,.8);
-  border-top: 1px solid #e7eaec;
-  overflow: hidden;
-  padding: 10px 20px;
-  height: 36px;
+.login-card >>> .ivu-input-prefix i {
+    font-size: 18px !important;
+    line-height: 45px !important;
 }
 </style>
